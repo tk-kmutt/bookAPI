cb025ef9366f8352448695e511224bacf22d871f
kuwabara <takashi.k@cayto.jp>
2021-06-22 14:43:09 +0900

    add health check

--- internal/http/server.go
+++ internal/http/server.go
@@ -1,9 +1,16 @@
 package http
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func Run() {
 	e := echo.New()
 
+	e.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "ok")
+	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
